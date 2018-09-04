79ac695a2c4b923c80a8f1de5500eb4a2d6d8b7e
angelav011301 <42008429+angelav011301@users.noreply.github.com>
2018-09-04 12:13:03 -0400

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,16 @@
+namesAges = {
+    'Angela' : 17,
+    'Kaylee' : 12,
+    'Oliver' : 21,
+    'Tiana' : 16,
+    'Rose' : 17,
+}
+
+# print (namesAges)
+
+for x in namesAges:
+    print (namesAges[x]) #prints values
+    print (x) #prints keys
+
+for x in namesAges:
+        print(x, namesAges[x])  #will print the Key and then the value in one line
